788d3c3d299b2ab2beb9f8960f2642a09dc0561f
JP Hastings-Spital <jphastings@gmail.com>
2021-06-01 22:32:44 +0100

    Fix entry points

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -e
 FIRMWAREURL="https://raw.githubusercontent.com/raspberrypi/firmware/master/boot/"
 
+KERNEL_ADDRESS=8000 # Also referenced in config.txt
+TEXT_START=00110000 # Space for interrupt vector, etc
+
 ROOT="$(pwd)"
 BUILD_DIR="${ROOT}/build"
 BUILD_TMP=$(mktemp -d)
@@ -25,15 +28,16 @@ export GOOS=tamago
 export GOARM=5
 export GOARCH=arm
 
-TEXT_START=0x00010000 # Space for interrupt vector, etc
+LDFLAGS="-s -w -T 0x${TEXT_START} -E _rt0_arm_tamago -R 0x1000 -X 'main.Build=tamago-build on $(/bin/date -u "+%Y-%m-%d %H:%M:%S")'"
+[[ -d "${SRC_DIR}/.git" ]] && LDFLAGS="${LDFLAGS} -X 'main.Revision=$(git rev-parse --short HEAD 2> /dev/null)'"
 
-/usr/local/tamago-go/bin/go build -ldflags "-s -w -T ${TEXT_START} -E _rt0_arm_tamago -R 0x1000" -o "${BUILD_TMP}/${APP}" "$SRC_DIR"
+/usr/local/tamago-go/bin/go build -ldflags "${LDFLAGS}" -o "${BUILD_TMP}/${APP}" "$SRC_DIR"
 objdump -D "${BUILD_TMP}/${APP}" > "${BUILD_TMP}/${APP}.list"
 
 objcopy -j .text -j .rodata -j .shstrtab -j .typelink \
   -j .itablink -j .gopclntab -j .go.buildinfo -j .noptrdata -j .data \
   -j .bss --set-section-flags .bss=alloc,load,contents \
-  -j .noptrbss --set-section-flags .noptrbss=alloc,load,contents \
+  -j .noptrbss --set-section-flags .noptrbss=alloc,load,contents\
   "${BUILD_TMP}/${APP}" -O binary "${BUILD_TMP}/${APP}.o"
 
 ENTRY_POINT=$(readelf -e "${BUILD_TMP}/${APP}" | grep Entry | sed 's/.*\(0x[a-zA-Z0-9]*\).*/\1/')
@@ -42,8 +46,9 @@ gcc -D ENTRY_POINT="${ENTRY_POINT}" -c /tamago-build/boot.S -o "${BUILD_TMP}/boo
 objcopy "${BUILD_TMP}/boot.o" -O binary "${BUILD_TMP}/stub.o"
 
 # Truncate pads the stub out to correctly align the binary
-# 32768 = 0x10000 (TEXT_START) - 0x8000 (Default kernel load address)
-truncate -s 32768 "${BUILD_TMP}/stub.o"
+# 1081344 = 0x110000 (TEXT_START) - 0x8000 (Default kernel load address)
+TRUNC=$(echo "ibase=16;${TEXT_START}-${KERNEL_ADDRESS}" | bc)
+truncate -s ${TRUNC} "${BUILD_TMP}/stub.o"
 
 cat "${BUILD_TMP}/stub.o" "${BUILD_TMP}/${APP}.o" > "${BUILD_DIR}/${APP}.bin"
 
